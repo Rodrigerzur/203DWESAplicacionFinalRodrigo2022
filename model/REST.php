7af5d6574896bb0d3c31ca7ba662176d9efa364e
Rodrigo <dragorodri@gmail.com>
2022-02-03 21:39:12 +0100

    03/02/2022 añadido + rest

--- model/REST.php
+++ model/REST.php
@@ -6,90 +6,14 @@ class REST {
 
      */
     public static function apod() {
-        $media = ''; # initialize empty item
-        $entry = ''; # initialize empty item
+        
         $api_key = 'wLlaZnbZhydhSJLcFAgF4Cdq8kfzRPArqb1GtwGF';
-        $apod_token = 'nasa_planetary_apod';
-
-        /**
-         * Check if the apod exists in the cache
-         */
-        /**
-         * If the item is not found in the cache
-         * then make request to the remote resource
-         * and push it to the cache system
-         */
-        $response = @file_get_contents("https://api.nasa.gov/planetary/apod?api_key="{$claveAPI});
-
-        /**
-         * If the response code is OK then save
-         * the input to Memcached for 1 hour
-         *
-         * The contents will be in JSON format
-         */
-        $apod = $response;
+        $response = @file_get_contents("https://api.nasa.gov/planetary/apod?api_key={$api_key}");
+        $JSONApod = json_decode($response, true);
+       
 
 
-
-
-
-        /**
-         * Unserialize the JSON to make an Object
-         */
-        $apod = json_decode($apod);
-
-        if (TRUE === is_object($apod)) {
-            $template = <<<'EOD'
-    <article>
-        <header>
-            <h2 class="text-center">Astronomy Picture of the Day</h2>
-        </header>
-        <figure>
-            <!-- media -->
-            %5$s
-            <figcaption>
-                <header>
-                    <p>%1$s</p>
-                    <h1>%2$s <small>&copy; %3$s</small></h1>
-                </header>
-                <main>
-                    <p>%4$s</p>
-                </main>
-                <footer>
-                    <p>More at <a href="%6$s">%6$s</a></p>
-                </footer>
-            </figcaption>
-        </figure>
-    </article>
-EOD;
-
-            if ('image' == $apod->media_type) {
-                $media_template = <<< 'EOD'
-            <a href="%s"><img src="%s" alt="%s"></a>
-EOD;
-                $media = sprintf($media_template, $apod->hdurl, $apod->url, $apod->title);
-            } elseif ('video' == $apod->media_type) {
-                $media_template = <<< 'EOD'
-            <iframe width="960" height="540" src="%s"></iframe>
-EOD;
-                $media = sprintf($media_template, $apod->url);
-            }
-
-            $date = (new IntlDateFormatter(Locale::getDefault(),
-                            IntlDateFormatter::LONG,
-                            IntlDateFormatter::LONG,
-                            date_default_timezone_get(),
-                            IntlDateFormatter::GREGORIAN))->format(new DateTime($apod->date));
-
-            $data = [$date
-                , mb_convert_case($apod->title, MB_CASE_TITLE, "UTF-8")
-                , $apod->copyright
-                , $apod->explanation
-                , $media
-                , 'http://apod.nasa.gov/apod/astropix.html'];
-
-            return $entry = vsprintf($template, $data);
-        }
+        return $JSONApod['url'];
     }
 
     /**
--- view/vREST.php
+++ view/vREST.php
@@ -5,59 +5,57 @@
     <input type="submit" value="Volver" name="volver" class="volver"/>
 </form>
 
-    <form name="formulario" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="form" id="formularioapod">
+<form name="formulario" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="form" id="formularioapod">
     <fieldset>
         <input type="submit" value="Mostrar APOD" name="mostrarAPOD" class="volver"/>
-        <?php
-        echo $iAPOD;
-    ?>
+        <img src=" <?php echo isset($iAPOD) ?> "> 
     </fieldset>
 </form>
 
-    <form name="formulario" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="form" id="formularioconversor">
-        <fieldset>
-            <p class="tituloiniciarsesion">Valor de Moneda<p>
-            
-                <p class="pInformacion">Permite pasar la cantidad 1 de la divisa que le pases en el primer campo a otra divisa que le pases en el segundo campo. El formato del campo es con el codigo de la moneda, el cual esta formado por tres letras. Ejemplo: EUR a USD.</p>
-            
-                <ul>
-                <!--Campo miDivisa OBLIGATORIO -->
-                <li>
-                    <div class="imageninformacion">
-                        <label for="miDivisa"><p class="pDivisa">Divisa*<a href="https://www.exchangerate-api.com/docs/standard-requests" target="_blank"></a></p></label>
-                        <div class="vercomousar">
-                            <p>Ejemplos de codigos de moneda disponibles: EUR: Euro, USD: Dolar, FJD: Dólar fiyiano, FKP: Libra malvinense ,GBP: Libra esterlina, JPY: Yen, LBP: Libra libanesa, MXN: Peso Mexicano, GIP: Libra de Gibraltar, CZK: Corona Checa, CNY: Yuan Chino, UYU: Peso Uruguayo, HUF: Forinto</p>
-                        </div>
-                        <input name="miDivisa" id="CodDivisa" type="text" value="<?php echo isset($_REQUEST['miDivisa']) ? $_REQUEST['miDivisa'] : null; ?>" placeholder="Codigo de Moneda">
-                        <p class="mensajeErrorRest"><?php echo $aErrores['miDivisa'] ?></p>
+<form name="formulario" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="form" id="formularioconversor">
+    <fieldset>
+        <p class="tituloiniciarsesion">Valor de Moneda<p>
+
+        <p class="pInformacion">Permite pasar la cantidad 1 de la divisa que le pases en el primer campo a otra divisa que le pases en el segundo campo. El formato del campo es con el codigo de la moneda, el cual esta formado por tres letras. Ejemplo: EUR a USD.</p>
+
+        <ul>
+            <!--Campo miDivisa OBLIGATORIO -->
+            <li>
+                <div class="imageninformacion">
+                    <label for="miDivisa"><p class="pDivisa">Divisa*<a href="https://www.exchangerate-api.com/docs/standard-requests" target="_blank"></a></p></label>
+                    <div class="vercomousar">
+                        <p>Ejemplos de codigos de moneda disponibles: EUR: Euro, USD: Dolar, FJD: Dólar fiyiano, FKP: Libra malvinense ,GBP: Libra esterlina, JPY: Yen, LBP: Libra libanesa, MXN: Peso Mexicano, GIP: Libra de Gibraltar, CZK: Corona Checa, CNY: Yuan Chino, UYU: Peso Uruguayo, HUF: Forinto</p>
                     </div>
-                </li>
-                <!--Campo Importe Conversion-->
-                <li>
-                    <div>
-                        <label for="cantidad"><p class="pDivisa">Cantidad</p></label>
-                        <input name="cantidad" id="Cantidad" type="text" value="1" readonly disabled>
-                    </div>
-                </li>
-                <!--Campo otraDivisa OBLIGATORIO-->
-                <li>
-                    <div>
-                        <label for="otraDivisa"><p class="pPasar">Pasar a*</p></label>
-                        <input name="otraDivisa" id="CodPasar" type="text" value="<?php echo isset($_REQUEST['otraDivisa']) ? $_REQUEST['otraDivisa'] : null; ?>" placeholder="Codigo de Moneda">
-                        <p class="mensajeErrorRest"><?php echo $aErrores['otraDivisa'] ?></p>
-                    </div>
-                </li>
-                <!--Campo Resultado Conversion-->
-                <li>
-                    <div>
-                        <label for="Resultado"><p class="pPasar">Resultado</p></label>
-                        <input name="Resultado" id="Resultado" type="text" value="<?php echo $iDevolucion ?>" readonly disabled>
-                    </div>
-                </li>
-                <!--Campo Boton CONVERTIR-->
-                <li>
-                    <input type="submit" value="CONVERTIR" name="convertir" class="convertir"/>
-                </li>
-            </ul>
-        </fieldset>
-    </form>
+                    <input name="miDivisa" id="CodDivisa" type="text" value="<?php echo isset($_REQUEST['miDivisa']) ? $_REQUEST['miDivisa'] : null; ?>" placeholder="Codigo de Moneda">
+                    <p class="mensajeErrorRest"><?php echo $aErrores['miDivisa'] ?></p>
+                </div>
+            </li>
+            <!--Campo Importe Conversion-->
+            <li>
+                <div>
+                    <label for="cantidad"><p class="pDivisa">Cantidad</p></label>
+                    <input name="cantidad" id="Cantidad" type="text" value="1" readonly disabled>
+                </div>
+            </li>
+            <!--Campo otraDivisa OBLIGATORIO-->
+            <li>
+                <div>
+                    <label for="otraDivisa"><p class="pPasar">Pasar a*</p></label>
+                    <input name="otraDivisa" id="CodPasar" type="text" value="<?php echo isset($_REQUEST['otraDivisa']) ? $_REQUEST['otraDivisa'] : null; ?>" placeholder="Codigo de Moneda">
+                    <p class="mensajeErrorRest"><?php echo $aErrores['otraDivisa'] ?></p>
+                </div>
+            </li>
+            <!--Campo Resultado Conversion-->
+            <li>
+                <div>
+                    <label for="Resultado"><p class="pPasar">Resultado</p></label>
+                    <input name="Resultado" id="Resultado" type="text" value="<?php echo $iDevolucion ?>" readonly disabled>
+                </div>
+            </li>
+            <!--Campo Boton CONVERTIR-->
+            <li>
+                <input type="submit" value="CONVERTIR" name="convertir" class="convertir"/>
+            </li>
+        </ul>
+    </fieldset>
+</form>
